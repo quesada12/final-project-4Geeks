c8215e6d5bcb9c53a48e8eba56f3e0debc9617d7
JimmyFV <jfletesv@hotmail.com>
2021-05-06 04:55:11 +0000

    Home page cambios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,63 +2,96 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container-fluid bg-white border rounded py-4 ">
-			<div className="jumbotron">
-				<h1>Hello World here i am</h1>
-			</div>
-
-			<div>
-				<h1>Alquilá tu Cancha en 3 pasos</h1>
-			</div>
-
-			<div className="card-deck">
-				<div className="card">
-					<img
-						src="https://image.freepik.com/foto-gratis/deportista-feliz-sonriendo-mirando-su-telefono-movil_362480-384.jpg"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Busca</h5>
-						<p className="card-text">
-							No llames más por teléfono! Conocé la disponibilidad de tus canchas preferidas o deja que la
-							app te recomiende el turno ideal por precio y ubicación.
-						</p>
+		<div>
+			<div className="jumbotron big-banner mb-0" style={{ height: "500px", padding: "150px 0 0 0" }}>
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col">
+							<div className="col-4">
+								<h1 className="display-5">Reservá tu cancha al instante</h1>
+								<p>Explorá las canchas disponibles en tu ciudad en tiempo real</p>
+								<Link to="/login">
+									<button className="btn btn-naranjaContraste btn-lg">¡Regístrate ya!</button>
+								</Link>
+							</div>
+						</div>
 					</div>
 				</div>
-
-				<div className="card">
-					<img
-						src="https://images.pexels.com/photos/399187/pexels-photo-399187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Reservá</h5>
-						<p className="card-text">
-							Compará precios, turnos, servicios del club. Tenés toda la información para hacer tu reserva
-							instantánea, sin pagar nada por adelantado!
-						</p>
-					</div>
+			</div>
+			<div className="container-fluid bg-white pt-3 pb-3">
+				<div className="text-verdeOscuro">
+					<h1 className="text-verdeOscuro mb-4">Alquilá tu Cancha en 3 pasos</h1>
 				</div>
 
-				<div className="card">
-					<img
-						src="https://images.pexels.com/photos/5886522/pexels-photo-5886522.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Jugá</h5>
-						<p className="card-text">
-							Pagá el 100% del turno en el club y disfrutá en la cancha! Vos preocupate por romperla en el
-							partido, nosotros nos ocupamos de la reserva ;)
-						</p>
+				<div className="card-deck text-justify">
+					<div className="card">
+						<img
+							src="https://image.freepik.com/foto-gratis/deportista-feliz-sonriendo-mirando-su-telefono-movil_362480-384.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Buscá</h5>
+							<p className="card-text">
+								¡No llames más por teléfono! Conocé la disponibilidad de tus canchas preferidas o dejá
+								que la app te recomiende el campo ideal por precio y ubicación.
+							</p>
+						</div>
+					</div>
+
+					<div className="card">
+						<img
+							src="https://images.pexels.com/photos/399187/pexels-photo-399187.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Reservá</h5>
+							<p className="card-text">
+								Compará precios, disponibilidad, servicios de la cancha. Tenés toda la información para
+								hacer tu reserva instantánea, sin pagar nada por adelantado!
+							</p>
+						</div>
+					</div>
+
+					<div className="card">
+						<img
+							src="https://images.pexels.com/photos/3148452/pexels-photo-3148452.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Jugá</h5>
+							<p className="card-text">
+								Pagá el 100% de tu campo mediante la app y disfrutá en la cancha! Preocupate por jugar
+								bien en el partido, nosotros nos ocupamos de tu reserva.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div>
+					<h1 className="text-verdeOscuro mb-3 mt-3">Canchas cerca de ti</h1>
+				</div>
+			</div>
+
+			<div className="jumbotron big-banner-map mb-0" style={{ height: "500px", padding: "150px 0 0 0" }}>
+				<div className="container">
+					<div className="row align-items-center">
+						<div className="col">
+							<div className="col-6">
+								<h1 className="display-5">Mirá las canchas dispobibles en tu localidad</h1>
+								{/* <p>Explorá las canchas disponibles en tu ciudad en tiempo real</p> */}
+								<Link to="/login">
+									<button className="btn btn-naranjaContraste btn-lg">¡Haz tu reserva!</button>
+								</Link>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
